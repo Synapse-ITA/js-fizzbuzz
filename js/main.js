976d2b117c73e2f2ab782806901f7b5edb8bd8c6
Synapse-ITA <puggillovalerio@gmail.com>
2023-11-15 16:17:02 +0100

    still working

--- js/main.js
+++ js/main.js
@@ -4,11 +4,17 @@
 
 for (let i = 1; i < 101; i++) {
     console.log(i)
-    
+
     if (i % 3 == 0) {
-        console.log(i + " fizz")
+        console.log(i + " fizz");
+    } else if (i % 5 == 0) {
+        console.log(i + " buzz");
     }
 }
+
+
+    
+
 
 
 
