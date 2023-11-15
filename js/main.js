845f8bcd9b0fc6b56e2f2c736974e6c015a007a2
Synapse-ITA <puggillovalerio@gmail.com>
2023-11-15 16:42:44 +0100

    maybe done

--- js/main.js
+++ js/main.js
@@ -3,13 +3,14 @@
 
 
 for (let i = 1; i < 101; i++) {
-    console.log(i)
     if (i % 15 == 0) {
         console.log(i + " fizzbuzz")
     } else if (i % 3 == 0) {
         console.log(i + " fizz");
     } else if (i % 5 == 0) {
         console.log(i + " buzz");
+    } else {
+        console.log(i)
     }
 }
 
